f8a627167adb3cd3e43f47e6597940c6978bd6ed
Adam Boinet <adam.boinet@gmail.com>
2020-06-25 19:34:24 +0200

    Moving 'twitter_store' in 'utils'

--- utils/twitter_store.py
+++ utils/twitter_store.py
@@ -5,23 +5,25 @@ try:
 
     CONSUMER_KEY = keys[0].strip("\n").split("=")
     assert CONSUMER_KEY[0] == "CONSUMER_KEY", "Bad format for CONSUMER_KEY"
-    CONSUMER_KEY = CONSUMER_KEY[1]
+    CONSUMER_KEY = CONSUMER_KEY[1].strip('"')
 
     CONSUMER_SECRET = keys[1].strip("\n").split("=")
     assert CONSUMER_SECRET[0] == "CONSUMER_SECRET", "Bad format for CONSUMER_SECRET"
-    CONSUMER_SECRET = CONSUMER_SECRET[1]
+    CONSUMER_SECRET = CONSUMER_SECRET[1].strip('"')
 
     ACCESS_TOKEN = keys[2].strip("\n").split("=")
     assert ACCESS_TOKEN[0] == "ACCESS_TOKEN", "Bad format for ACCESS_TOKEN"
-    ACCESS_TOKEN = ACCESS_TOKEN[1]
+    ACCESS_TOKEN = ACCESS_TOKEN[1].strip('"')
 
     ACCESS_TOKEN_SECRET = keys[3].strip("\n").split("=")
     assert (
         ACCESS_TOKEN_SECRET[0] == "ACCESS_TOKEN_SECRET"
     ), "Bad format for ACCESS_TOKEN_SECRET"
-    ACCESS_TOKEN_SECRET = ACCESS_TOKEN_SECRET[1]
+    ACCESS_TOKEN_SECRET = ACCESS_TOKEN_SECRET[1].strip('"')
 except:
-    raise Exception("[ERROR] Couldn't load 'twitter_keys.txt'")
+    msg = "\n\n\033[1m\033[91m[ERROR] Couldn't retrieve Twitter API Tokens\033[0m\n\n"
+    msg += "\033[91mDid you forget to create 'utils/twitter_keys.txt'?\033[0m"
+    raise Exception(msg)
 
 
 class TwitterStore:
