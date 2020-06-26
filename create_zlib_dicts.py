f438c5335ffa73d8a4cd8ebbda60c5570b863a2c
Adam Boinet <adam.boinet@gmail.com>
2020-06-26 19:38:05 +0200

    Adding zlib dict generation script

---
+++ create_zlib_dicts.py
@@ -0,0 +1,42 @@
+from utils.utils import generate_distrib_dict, get_list_from_file
+
+liste_regions = [
+    "Auvergne-Rhône-Alpes",
+    "Bourgogne-Franche-Comté",
+    "Bretagne",
+    "Centre-Val de Loire",
+    "Corse",
+    "Grand Est",
+    "Guadeloupe",
+    "Guyane",
+    "Hauts-de-France",
+    "Ile-de-France",
+    "La Réunion",
+    "Martinique",
+    "Mayotte",
+    "Normandie",
+    "Nouvelle-Aquitaine",
+    "Occitanie",
+    "Pays de la Loire",
+    "Provence-Alpes-Côte d'Azur",
+]
+
+regions_dict = {}
+
+for region in liste_regions:
+    data = get_list_from_file(
+        "./utils/regions_france/" + region.replace("'", "-") + ".txt"
+    )
+    data.pop()
+    print(len(data), region)
+    regions_dict[region] = data
+
+print("\nStarting dict generation\n")
+
+for region in regions_dict.keys():
+    print(f"Generation dict for {region}")
+    substitute_name = region.replace("'", "-")
+    generate_distrib_dict(
+        name_list=regions_dict[region],
+        file_path=f"./utils/regions_france_dict/distrib_dict_{substitute_name}.zlib",
+    )
