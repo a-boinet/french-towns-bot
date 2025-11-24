05351bb771e1e2e01949e2f943ddb6eac44bc89e
Adam Boinet <adam.boinet@gmail.com>
2025-11-24 17:00:08 +0100

    Add graceful termination for playground.py

--- playground.py
+++ playground.py
@@ -1,6 +1,17 @@
 from src.generator import generate_tweet
 
 if __name__ == "__main__":
-    while True:
-        print(generate_tweet()[0])
-        input("\033[90mPress enter to generate a new city (CTRL + C to quit)\033[0m")
+    names_generated = 0
+    try:
+        while True:
+            print(generate_tweet()[0])
+            names_generated += 1
+            input(
+                "\033[90mPress enter to generate a new city (CTRL + C to quit)\033[0m"
+            )
+    except KeyboardInterrupt:
+        print(
+            f"\n\nYou generated {names_generated} "
+            f"{'city' if names_generated <= 1 else 'cities'} "
+            f"- See you soon!\n"
+        )
